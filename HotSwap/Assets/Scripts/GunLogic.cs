0c39fdd955ddfc584b1046e0e4aa1c6484559477
Matthew Gonzales <matthekg@uci.edu>
2019-05-11 01:17:41 -0700

    Added a Basic Level and Hero
    Hero
    +Can move in a 2D space with WASD or Joystick
    +Can aim a gun using the Mouse

---
+++ HotSwap/Assets/Scripts/GunLogic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunLogic : MonoBehavioura
+{
+    [Header("GunMovement")]
+    public float armLength = 1f;
+    private Transform rotationPoint;
+
+
+    void Start()
+    {
+        rotationPoint = transform.parent.transform;    
+    }
+
+    void Update()
+    {
+        // Get direction from player to mouse
+        Vector3 centerToMouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - rotationPoint.position;
+
+
+        // This puts the gun right above the player on the z-axis
+        centerToMouseDir.z = -.43f;
+
+        // Put the gun in that direction, as far as the armlength
+        transform.position = rotationPoint.position + (armLength * centerToMouseDir.normalized);
+    }
+}
---
+++ HotSwap/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+    [Header("Movement Stats")]
+    [SerializeField] float speed = 5f;
+    [SerializeField] float dodgeSpeed = 0f;
+    [SerializeField] float dodgeDistance = 0f;
+    [SerializeField] int dodgeIFrames = 0;
+
+
+    private Rigidbody2D rb;
+
+    [Header("Controls")]
+    public string horizontalControl = "Horizontal";
+    public string verticalControl = "Vertical";
+    public string shootControl = "Fire1";
+    public string dodgeControl = "Jump";
+    [SerializeField] private bool rawInputOn = false;
+    private float xInput = 0;
+    private float yInput = 0;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if ( Input.GetAxisRaw(dodgeControl) > 0 )
+            Dodge();
+    }
+
+    void FixedUpdate()
+    {
+        if (rawInputOn) {
+            xInput = Input.GetAxisRaw(horizontalControl);
+            yInput = Input.GetAxisRaw(verticalControl);
+        }
+        else {
+            xInput = Input.GetAxis(horizontalControl);
+            yInput = Input.GetAxis(verticalControl);
+        }
+
+        rb.velocity = new Vector2(xInput * speed, yInput * speed);
+    }
+
+    private void Dodge( /*direction?*/ )
+    {
+
+    }
+}
