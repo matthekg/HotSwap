8dc1eaa93e07f007c089346030fd6cf18473d26c
Matthew Gonzales <matthekg@uci.edu>
2019-06-12 16:04:14 -0700

    Merged Network into Master

---
+++ HotSwap/Assets/NetScripts/NetworkCallbacks.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+	public override void SceneLoadLocalDone(string scene){
+		var spawnPosition = new Vector2(-10,0);
+		BoltNetwork.Instantiate(BoltPrefabs.Square,spawnPosition,Quaternion.identity);
+	}
+}
